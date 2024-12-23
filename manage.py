19d91a61b982851fc8da5540b7916b9af56c495e
Alexander Zhang <alexyz@mit.edu>
2024-12-22 22:29:50 -0800

    updated requirements

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 from server import app, socketio, db
 from flask_script import Manager
 from flask_migrate import MigrateCommand
+from flask_cors import CORS
+
+CORS(app)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 @manager.command
 def runserver():
-    socketio.run(app, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0', port=5001)
 
 if __name__ == '__main__':
     manager.run()

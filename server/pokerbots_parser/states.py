5127080578187c23e4f0c94d20a885d1dec39aea
gaurab1 <gaurabdps@gmail.com>
2024-12-28 20:57:27 -0600

    Minor fix based on engine updates

--- server/pokerbots_parser/states.py
+++ server/pokerbots_parser/states.py
@@ -32,7 +32,7 @@ class RoundState(namedtuple('_RoundState', ['button', 'street', 'pips', 'stacks'
                 - First boolean indicates if Player 1's bounty was hit
                 - Second boolean indicates if Player 2's bounty was hit
         '''
-        cards = self.hands[0] + self.hands[1] + ([] if self.street == 0 else self.deck.peek(self.street))
+        cards = self.hands[0] + self.hands[1] + ([] if self.street == 0 else self.deck[:self.street])
         ranks = {'2':0, '3':1, '4':2, '5':3, '6':4, '7':5, '8':6, '9':7, 'T':8, 'J':9, 'Q':10, 'K':11, 'A':12}
         return (self.bounties[0] in [ranks[card[0]] for card in cards],
                 self.bounties[1] in [ranks[card[0]] for card in cards])
